a8a9acd3f3a6975bb35cf3c3dc8b1ec97a76ba74
Jeremy Menetrier (Selectra) <jeremy.menetrier@gmail.com>
2015-03-26 10:36:20 +0100

    Update GH refs

--- definitions/services.js
+++ definitions/services.js
@@ -5,7 +5,7 @@ exports.root = {
 exports.web = {
   type: 'docker',
   specific: {
-    repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
+    repositoryUrl: 'git@github.com:menetrier/demoweb.git',
     execute: {
       args: '-p 8000:8000 -d',
       exec: ''
